c6e5b1a8a245a138d3516217ba6e4aa579ff945e
Frank Greco <gre9521@s238141.local>
2016-09-18 15:40:32 -0500

    Adding License file

---
+++ README.md
@@ -0,0 +1,14 @@
+# Average Words Per Sentence
+
+## Overview
+
+This **C** program computes the average number of words per senctence.
+
+## Quickstart
+
+Provided is a test file that contains the text for the Preamble of the *U.S. Declaration of Independence*.
+
+1. Navigate to the project directory
+2. Run `./main test.txt`
+
+**Output:** `Your body of text contains 201 words and 4 sentences resulting in an average of 50.25 words per sentence`
--- count.c
+++ count.c
@@ -6,13 +6,29 @@
 
 void count_sentences(char* buffer, int* total_sentences, int* total_words) {
 
-	char** sentences = malloc(sizeof(char*) * 2048);
-
+	//Current number of spaces we have allocated in our array or char arrays.
+	int cur_allocated = 100;
+	//Allocating memory for an array of char arrays. Initally guessing that 100 char arrays will be needed
+	char** sentences = malloc(sizeof(char*) * cur_allocated);
+	//Points to the current sentence
 	char* tmp_sentence;
+	//Attempt to grab the first sentece from the buffer.
 	tmp_sentence = strtok(buffer, ".?!");
+
 	while( tmp_sentence != NULL ) {
+		if( (*total_sentences + 1) > cur_allocated ) {
+			//double the size
+			cur_allocated *= 2;
+			//grow the size of the array
+			sentences = realloc(sentences, sizeof(char*) * cur_allocated);
+		}
 		sentences[(*total_sentences)++] = strcpy((char*)malloc(sizeof(char) * (strlen(tmp_sentence) + 1)), tmp_sentence);
 		tmp_sentence = strtok(NULL, ".?!");
+	}
+
+	//done reading from buffer. Shrink the array if necessary.
+	if(*total_sentences > cur_allocated) {
+		sentences = realloc(sentences, sizeof(char*) * *total_sentences);
 	}
 
 	int i = 0;
